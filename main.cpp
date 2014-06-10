f7a7a77099024586437aa600b0c73f9066ae3898
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-06-10 23:04:20 +0100

    working on basic functions

--- main.cpp
+++ main.cpp
@@ -1,3 +1,36 @@
-#include <stdio>
-A
+#include <iostream>
+#include "page.hpp"
+#include "expat.h"
 
+use namespace std;
+
+
+void usage()
+{
+	cout << "japa - just another paging analyser\n";
+	cout << "Basic usage:\n";
+	cout << "\tjapa inputfile\n";
+	cout << "Options:\n";
+	cout << "\t-i\t\tIntensity (default)\n";
+	cout << "\t-ni\t\tDo not measure intensity\n";
+	return;
+}
+
+int main(int argc, char *argv[])
+{
+	bool intensity = true; //default use case
+	bool breadth = false;
+	
+	if (argc < 2) {
+		usage();
+		return 1;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		if ((strcmp(argv[i], "-i") == 0) {
+			intensity = true;
+		}
+
+		if (strcmp(argv[i], "-ni") == 0) {
+			intensity = false;
+		}
