9e39de280e73fa0a1cb6daced8ab24107d25449c
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-06-12 23:43:20 +0100

    working on

--- main.cpp
+++ main.cpp
@@ -13,13 +13,39 @@ void usage()
 	cout << "Options:\n";
 	cout << "\t-i\t\tIntensity (default)\n";
 	cout << "\t-ni\t\tDo not measure intensity\n";
+	cout << "\t-?\t\tShow this usage message\n";
 	return;
 }
+
+static void XMLCALL
+intensityHandler(void* data, const XML_Char *name, const XML_Char **attr)
+{
+	long in, out, frame;
+	if (strcmp("name", "page") == 0) {
+		for (int i = 0; attr[i]; i+=2) {
+			if (strcmp(attr[i], "frame") == 0) {
+				frame = atol(attr[i + 1]);
+				continue;
+			}
+			if (strcmp(attr[i], "in") == 0) {
+				in = atol(attr[i + 1]);
+				continue;
+			}
+			if (strcmp(attr[i], "out") == 0) {
+				out = atol(attr[i + 1]);
+			}
+		}
+		//set new handler
+
 
 int main(int argc, char *argv[])
 {
 	bool intensity = true; //default use case
 	bool breadth = false;
+	char *xmlFile;
+	FILE *inXML;
+	size_t len = 0;
+	char data[BUFFSZ];
 	
 	if (argc < 2) {
 		usage();
@@ -30,7 +56,53 @@ int main(int argc, char *argv[])
 		if ((strcmp(argv[i], "-i") == 0) {
 			intensity = true;
 		}
-
 		if (strcmp(argv[i], "-ni") == 0) {
 			intensity = false;
 		}
+		if (strcmp(argv[i], "-?") == 0) {
+			usage();
+			exit(0);
+		}
+	}
+
+	xmlFile = argv[argc - 1];
+
+	XML_Parser p_ctrl = XML_ParserCreate("UTF-8");
+	if (p_ctrl) {
+		cout << "Could not create XML parser.\n";
+		return 1;
+	}
+
+	if (intensity) {
+		XML_SetStartElementHandler(p_ctrl, intensityHandler);
+		inXML = fopen(xmlFile, "r");
+		if (!inXML) {
+			cout << "ERROR: could not open ";
+			cout << xmlFile;
+			XML_ParserFree(p_ctrl);
+			return 1;
+		}
+
+		do {
+			len = fread(data, 1, sizeof(data), inXML);
+			done = len < sizeof(data);
+			if (XML_Parse(p_ctrl, data, len, 0) == 0) {
+				enum XML_Error = XML_GetErrorCode(p_ctrl);
+				cout << "ERROR: " << XML_ErrorString(errcode);
+				cout << "\n";
+				cout << "Error at line, column ";
+				cout << XML_GetCurrentLineNumber(p_ctrl);
+				cout << ",";
+				cout << XML_GetCurrentColumnNumber(p_ctrl);
+				cout << XML_ParserFree(p_ctrl) << "\n";
+				return 1;
+			}
+		} while(!done);
+	}
+
+	XML_ParserFree(p_ctrl);
+	fclose(inXML);
+
+	return 0;
+}
+
